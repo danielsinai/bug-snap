caba43b5ce721430252bd0ef911db4735884f738
Daniel Sinai <daniel.sinai.main@gmail.com>
2024-07-08 21:07:26 +0300

    fix: destroying window on quite

--- electron/index.ts
+++ electron/index.ts
@@ -91,6 +91,7 @@ function createWindow() {
         label: 'Quit',
         click: () => {
           app.quit();
+          window.destroy();
         }
       }
     ]);
@@ -166,9 +167,9 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') app.quit();
-// });
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
